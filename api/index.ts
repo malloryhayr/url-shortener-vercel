e9620e646dc120ab7feda12cc74899db4758a415
iGalaxyYT <contact@igalaxy.dev>
2021-02-23 17:21:26 -0800

    Final fix hopefully

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,16 @@
 import { NowRequest, NowResponse } from '@vercel/node';
 
+const routes: any = {
+  twitter: 'https://twitter.com/_iGalaxyYT',
+  youtube: 'https://youtube.com/c/WhiteGalaxyOfficial',
+  twitch: 'https://twitch.tv/igalaxyofficial',
+  github: 'https://github.com/iGalaxyYT',
+  source: 'https://github.com/iGalaxyYT/url-shortener-vercel',
+  porpoise: 'https://github.com/porpoise/porpoise',
+  typeskript: 'https://github.com/TypeSkript'
+}
+
 export default (req: NowRequest, res: NowResponse) => {
-  res.send({query: req.query});
+  const route = routes[req.query.route as string] || 'https://igalaxy.dev';
+  res.redirect(route);
 }
