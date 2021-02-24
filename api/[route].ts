c5321ed8842508aca46ed4efc252ad58552d62ed
iGalaxyYT <contact@igalaxy.dev>
2021-02-23 16:51:34 -0800

    Add debug

--- api/[route].ts
+++ api/[route].ts
@@ -12,5 +12,6 @@ const routes: any = {
 
 export default (req: NowRequest, res: NowResponse) => {
   const route = routes[req.query.route as string] || 'https://igalaxy.dev';
+  console.log(route);
   res.redirect(route);
 }
