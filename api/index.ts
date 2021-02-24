c703be15faf89bcc8543c523eecf4ddb627b3479
iGalaxyYT <contact@igalaxy.dev>
2021-02-23 17:17:43 -0800

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 import { NowRequest, NowResponse } from '@vercel/node';
 
 export default (req: NowRequest, res: NowResponse) => {
-  res.redirect('https://igalaxy.dev?redirect=true');
+  res.redirect(`https://igalaxy.dev?redirect=true&query=${req.query}`);
 }
