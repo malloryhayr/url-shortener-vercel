0dda392a8e5a873efd52dd2287093bfccafd7f33
iGalaxyYT <contact@igalaxy.dev>
2021-02-23 16:50:14 -0800

    Complete rewrite

---
+++ api/[route].ts
@@ -0,0 +1,16 @@
+import { NowRequest, NowResponse } from '@vercel/node';
+
+const routes: any = {
+  twitter: 'https://twitter.com/_iGalaxyYT',
+  youtube: 'https://youtube.com/c/WhiteGalaxyOfficial',
+  twitch: 'https://twitch.tv/igalaxyofficial',
+  github: 'https://github.com/iGalaxyYT',
+  source: 'https://github.com/iGalaxyYT/url-shortener-vercel',
+  porpoise: 'https://github.com/porpoise/porpoise',
+  typeskript: 'https://github.com/TypeSkript'
+}
+
+export default (req: NowRequest, res: NowResponse) => {
+  const route = routes[req.query.route as string] || 'https://igalaxy.dev';
+  res.redirect(route);
+}
--- api/index.ts
+++ api/index.ts
@@ -1,52 +1,5 @@
-import express from 'express';
-import fs from "fs";
-import path from "path";
+import { NowRequest, NowResponse } from '@vercel/node';
 
-const app = express();
-
-interface Route {
-    path: string,
-    result: string
+export default (req: NowRequest, res: NowResponse) => {
+    res.redirect('https://igalaxy.dev');
 }
-
-interface ServerOptions {
-    port?: number,
-    defaultRedirect?: string
-}
-
-class Server {
-    private port: number = 8080;
-    private defaultRedirect: string = 'https://igalaxy.dev';
-
-    constructor(options?: ServerOptions){
-        this.port = options?.port || this.port;
-        this.defaultRedirect = options?.defaultRedirect || this.defaultRedirect;
-    }
-
-    public register = (route: Route) => {
-        app.get(`/${route.path}`, (req, res) => {
-            return res.redirect(route.result);
-        });
-    }
-
-    public start = () => {
-        app.get('*', (req, res) => {
-            return res.redirect(this.defaultRedirect);
-        });
-
-        app.listen(this.port, () => {
-            console.log(`URL shortener started on port ${this.port}`);
-        });
-    }
-}
-
-let port = 8080;
-let defaultRedirect = 'https://igalaxy.dev';
-
-const server = new Server({port: port, defaultRedirect: defaultRedirect});
-
-const routes = JSON.parse(fs.readFileSync(path.join(__dirname, '_files', 'routes.json')).toString());
-routes.forEach((route: Route) => {
-    server.register(route);
-});
-server.start();
